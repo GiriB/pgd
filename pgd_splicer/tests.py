56a421b7ccf70df177624ca740ca043f93495bc1
Giri Bhatnagar <Girigaurav_bhatnagar@intuit.com>
2017-02-26 22:59:41 +0530

    Adds tests for valid occupancy values

--- pgd_splicer/tests.py
+++ pgd_splicer/tests.py
@@ -417,9 +417,9 @@ class ProcessPDBTask(TestCase):
                         self.fail("%s %s res %s: ome value invalid: %f" % (p.code, c.code, r.chainIndex, r.ome))
                     if r.omep is not None and abs(r.omep) >= 180.0:
                         self.fail("%s %s res %s: omep value invalid: %f" % (p.code, c.code, r.chainIndex, r.omep))
-                    if r.occm <=1 and r.occm >=1 and r.occm is not None:
+                    if not (r.occm <= 1.0 and r.occm >= 0.0 and r.occm is not None):
                         self.fail("%s %s res %s: occm value invalid: %f" % (p.code, c.code, r.chainIndex, r.occm))
-                    if r.occscs <=1 and r.occscs >=1 and r.occscs is not None:
+                    if not (r.occscs <= 1.0 and r.occscs >= 0.0 and r.occscs is not None):
                         self.fail("%s %s res %s: occscs value invalid: %f" % (p.code, c.code, r.chainIndex, r.occscs))
                     if r.aa == "Gly" and r.occscs != 1.0:
                         self.fail("%s %s res %s: occscs value invalid for Gly: %f" % (p.code, c.code, r.chainIndex, r.occscs))
